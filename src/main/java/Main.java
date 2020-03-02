e6a374301bd74bcec360a77665a751d1538e92a6
yogesh <yogeshgilly@gmail.com>
2020-03-02 22:04:12 +0530

    publish video changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import helpers.Base;
 import org.openqa.selenium.Alert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,52 +14,58 @@ public class Main extends Base {
         super(driver);
     }
 
-    @Test(priority = 0)
-    public void getLink(){
-        driver.get("https://www.hippovideo.io");
+    @Test
+    @Parameters({"url"})
+    public void getLink(String url){
+        driver.get(url);
     }
-//
+
+
+    @Test
+    @Parameters({"mail","password"})
+    public void signUp(String mailId,String password){
+        SignUpBusiness signUpBusiness=new SignUpBusiness(driver,"akselvan44@gmail.com","1028ksjr");
+        signUpBusiness.signUpWithNewUser();
+    }
+
+    @Test
+    @Parameters({"plan","name","company","phNo"})
+    public void fillBusinessDetails(String plan,String name,String company,String phNo){
+        EnterBusinessDetails businessDetails=new EnterBusinessDetails(driver,plan,name
+                                              ,company,phNo);
+        businessDetails.getPlan();
+        businessDetails.fillBusinessDetails();
+    }
+
 //    @Test(priority = 1)
-//    public void signUp(){
-//        SignUpBusiness signUpBusiness=new SignUpBusiness(driver,"akselvan44@gmail.com","ksjrksjrks");
-//        signUpBusiness.signUpWithNewUser();
-//    }
-//
-//    @Test(priority = 2)
-//    public void fillBusinessDetails(){
-//        EnterBusinessDetails businessDetails=new EnterBusinessDetails(driver,"business","artist"
-//                                              ,"bubble","9787086356");
-//        businessDetails.getPlan();
-//        businessDetails.fillBusinessDetails();
+//    public void clidk(){
+//        LoginPage loginPage=new LoginPage(driver);
+//        loginPage.clickLoginButton();
+//        loginPage.setUsernameAndPassword("akselvan44@gmail.com","ksjrksjrks");
 //    }
 
-    @Test(priority = 1)
-    public void clidk(){
-        LoginPage loginPage=new LoginPage(driver);
-        loginPage.clickLoginButton();
-        loginPage.setUsernameAndPassword("akselvan44@gmail.com","ksjrksjrks");
-    }
-
-    @Test(priority = 2)
-    public void startVideo() throws InterruptedException {
+    @Test
+    @Parameters({"videoDuration"})
+    public void startVideo(String duration) throws InterruptedException {
         RecordVideo recordVideo=new RecordVideo(driver);
         recordVideo.startCreatingVideo();
-        Thread.sleep(10000);
+        Thread.sleep(3000);
         recordVideo.startRecord();
-        recordVideo.stopRecording(2);
+        recordVideo.stopRecording(Integer.parseInt(duration));
     }
 
-    @Test(priority = 3)
+    @Test
     public void personalizeVideo(){
         PersonalizeVideo personalizeVideo=new PersonalizeVideo(driver);
         personalizeVideo.personalizeVideo();
     }
 
-    @Test(priority = 4)
-    public void publishVideo(){
+    @Test
+    @Parameters({"publishMail","publishFirstName","publishSubject"})
+    public void publishVideo(String publishMail,String publishFirstName,String publishSubject){
         PublishVideo publishVideo=new PublishVideo(driver);
         publishVideo.startVideoCampaign();
+        publishVideo.addContactAndSendMail(publishMail,publishFirstName,publishSubject);
     }
-
 
 }
--- src/main/java/pageobjects/PublishVideo.java
+++ src/main/java/pageobjects/PublishVideo.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 
 public class PublishVideo extends Base {
     public PublishVideo(WebDriver driver) {
-        this.driver=driver;
-        PageFactory.initElements(driver,this);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(id = "videoPublishTab-heading")
@@ -20,10 +20,10 @@ public class PublishVideo extends Base {
     @FindBy(xpath = "//input[@placeholder='Enter email id']")
     WebElement emailTextBox;
 
-    @FindBy(id="emailTo")
+    @FindBy(id = "emailTo")
     WebElement mailTo;
 
-    @FindBy(id="emailSubject")
+    @FindBy(id = "emailSubject")
     WebElement subject;
 
     @FindBy(id = "emailSendBtn")
@@ -35,64 +35,102 @@ public class PublishVideo extends Base {
     @FindBy(id = "videoCampaignsTab-heading")
     WebElement videoCampaignTab;
 
-    @FindBy(id="videoCampaign")
+    @FindBy(id = "videoCampaign")
     WebElement videoCampaign;
 
-    @FindBy(id="grantAccessBtn")
+    @FindBy(id = "grantAccessBtn")
     WebElement configureMail;
 
     @FindBy(id = "configureGmailSettingsBtn")
     WebElement gmailConfigure;
 
-    @FindBy(id = "identifierId")
-    WebElement gmailTxtBox;
+    @FindBy(xpath = "//input[@type='email']")
+    //@FindBy(id = "identifierId")
+            WebElement gmailTxtBox;
 
+    //@FindBy(xpath = "//input[@type='email']")
     @FindBy(id = "identifierNext")
     WebElement gmailNext;
 
-    @FindBy(id = "password")
-    WebElement gmailPassword;
+    @FindBy(xpath = "//input[@type='password']")
+    //@FindBy(id = "identifierNext")
+            WebElement gmailPassword;
 
-    @FindBy(id="passwordNext")
+    @FindBy(id = "passwordNext")
     WebElement gmailPwdNext;
 
-    public void publishVideo(){
+
+    @FindBy(id = "addContacts")
+    WebElement addContacts;
+
+    @FindBy(xpath = "//span[@class='add-contacts-btn']")
+    WebElement addContactToMail;
+
+    @FindBy(xpath = "//div[@class='large-3 columns direct-contacts-email']/input[1]")
+    WebElement mailId;
+
+    @FindBy(xpath = "//div[@class='large-9 columns hp-custom-scromm ac-parent direct-contacts-mfields']/div[1]/table/tbody/tr/td/input")
+    WebElement firstName;
+
+    @FindBy(id = "emailSubject")
+    WebElement mailSubject;
+
+    @FindBy(id = "sendEmailBtn")
+    WebElement mailSend;
+
+
+    public void publishVideo() {
         clickElement(publishTab);
-        if (isDisplayed(emailTextBox)){
+        if (isDisplayed(emailTextBox)) {
             emailTextBox.sendKeys("klisvn@gmail.com");
-            if (isDisplayed(subject)){
+            if (isDisplayed(subject)) {
                 subject.sendKeys("Summa purpose");
                 sendMail.click();
             }
         }
-        if (isDisplayed(mailSucessTag)){
+        if (isDisplayed(mailSucessTag)) {
             System.out.println("Mail Sent Sucessfully");
         }
     }
 
 
-    public void startVideoCampaign(){
+    public void startVideoCampaign() {
         clickElement(videoCampaignTab);
         clickElement(videoCampaign);
         clickElement(configureMail);
         configureGmail();
     }
 
-    private void configureGmail(){
-        ArrayList<String> tabs=new ArrayList<String>(driver.getWindowHandles());
-        driver.switchTo().window(tabs.get(1));
-            clickElement(gmailConfigure);
-            ArrayList<String> mailtabs=new ArrayList<String>(driver.getWindowHandles());
-        driver.switchTo().window(mailtabs.get(2));
-            if (isDisplayed(gmailTxtBox)){
-                gmailTxtBox.sendKeys("klisvn@gmail.com");
-                clickElement(gmailNext);
-                if (isDisplayed(gmailPassword)){
-                    gmailPassword.sendKeys("ksjrgm1028");
-                    clickElement(gmailPwdNext);
-                }
+
+    public void addContactAndSendMail(String mail, String fName, String mailSub) {
+        clickElement(addContactToMail);
+        if (isDisplayed(mailId)) {
+            mailId.sendKeys(mail);
+            if (isDisplayed(firstName)) {
+                firstName.sendKeys(fName);
+                clickElement(addContacts);
             }
+        }
+        if (isDisplayed(mailSubject)) {
+            mailSubject.sendKeys(mailSub);
+            clickElement(mailSend);
+        }
+    }
 
 
+    private void configureGmail() {
+        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        clickElement(gmailConfigure);
+        ArrayList<String> mailtabs = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(mailtabs.get(2));
+        if (isDisplayed(gmailTxtBox)) {
+            gmailTxtBox.sendKeys("klisvn@gmail.com");
+            clickElement(gmailNext);
+            if (isDisplayed(gmailPassword)) {
+                gmailPassword.sendKeys("ksjrgm1028");
+                clickElement(gmailPwdNext);
+            }
+        }
     }
 }
